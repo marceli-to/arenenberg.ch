edfdb0251b4883619d1bd9405ab5dce251bfbf53
Marcel Stadelmann <m@marceli.to>
2024-11-23 13:12:52 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: 'arenenberg.ch.test',
     port: 5173
+  },
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.esm-bundler.js',
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
