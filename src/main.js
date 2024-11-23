ef3ebc25d14d0407794e6db1abdd626a78e9abae
Marcel Stadelmann <m@marceli.to>
2024-11-23 14:55:03 +0100

    wip

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import './registerServiceWorker'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
