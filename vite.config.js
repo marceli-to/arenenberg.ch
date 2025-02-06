22ffd166f476d65a95425ddd3cdaf30d05e643a9
Marcel Stadelmann <m@marceli.to>
2025-02-06 09:21:28 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Unterwegs am Arenenberg',
+        short_name: 'Arenenberg',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     host: 'arenenberg.ch.test',
     port: 5173
