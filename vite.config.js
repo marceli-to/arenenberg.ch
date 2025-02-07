c8b5ebbb684c64fa9013c0f6edabb99dbfaf78ad
Marcel Stadelmann <m@marceli.to>
2025-02-07 13:31:02 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     laravel({
       input: [
         'resources/css/app.css',
-        'resources/js/app.js',
+        //'resources/js/app.js',
         'resources/js/db.js',
         'resources/js/sw.js',
       ],
