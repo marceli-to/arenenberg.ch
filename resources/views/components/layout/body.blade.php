82a5452cfd6eb3ef4264164857151f8908838d42
Marcel Stadelmann <m@marceli.to>
2025-02-07 15:00:19 +0100

    wip

--- resources/views/components/layout/body.blade.php
+++ resources/views/components/layout/body.blade.php
@@ -1,10 +1,8 @@
 <body class="bg-white text-black text-sm antialiased leading-[1.25] flex min-h-screen flex-col max-w-lg mx-auto">
 {{ $slot }}
-
-@if (!Request::routeIs('page.station.*'))
-  @vite('resources/js/db.js')
-  @vite('resources/js/main.js')
-@endif
+<script src="/js/db.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/station.js"></script>
 </body>
 </html>
 <!-- made with ❤ by bivgrafik.ch & marceli.to -->
